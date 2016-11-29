d5f3cae5f7ce0ddccdf8fbdfbc641b46702119a6
Jeroen Engels <jfm.engels@gmail.com>
2016-11-29 13:27:06 +0100

    tmp test

--- packages/@coorpacademy-components/src/util/test/validation/validate.js
+++ packages/@coorpacademy-components/src/util/test/validation/validate.js
@@ -1,14 +1,40 @@
 import test from 'ava';
 import isFunction from 'lodash/fp/isFunction';
-import {checker, createValidate} from '../../validation';
+import * as treant from '@coorpacademy/treantjs-core';
+import {checker, validate} from '../../validation';
 
-test('should provide a validate function with warn', t => {
-  const conditions = checker.shape({});
-  const validate = createValidate(conditions);
-  t.true(isFunction(validate));
+const {h, resolve} = treant;
+
+test('should extend children properties', t => {
+  const Title = (props, children) => <h1 style={{color: props.color}}>{children}</h1>;
+  const Color = createWrapper(() => props => ({color: props.color}))(treant);
+
+  const tree = (
+    <Color color="blue">
+      <Title>foo</Title>
+    </Color>
+  );
+
+  t.deepEqual(resolve(tree), resolve(<Title color="blue">foo</Title>));
+  t.deepEqual(resolve(tree), <h1 style={{color: 'blue'}}>foo</h1>);
+});
+
+test('should create a function wrapping a component', t => {
+  const conditions = checker.shape({value: checker.string});
+  const Component = (props, children) => 'success';
+
+  t.true(isFunction(validate(conditions, Component)));
+});
+
+test('should create a function wrapping a component', t => {
+  const conditions = checker.shape({value: checker.string});
+  const Component = (props, children) => 'success';
+
+  t.true(isFunction(validate(conditions, Component)));
 });
 
 test('should provide a validate function with throw', t => {
+
   const conditions = checker.shape({});
   const validate = createValidate(conditions, true);
   t.true(isFunction(validate));
