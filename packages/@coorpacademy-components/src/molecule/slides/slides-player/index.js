0e37342ad2eb833402c24777a18b32c1498db146
Joan Ortega <jomaora@gmail.com>
2017-06-14 12:14:18 +0200

    Adding clue prop to slide and fixtures

--- packages/@coorpacademy-components/src/molecule/slides/slides-player/index.js
+++ packages/@coorpacademy-components/src/molecule/slides/slides-player/index.js
@@ -66,13 +66,35 @@ const createStepView = (step, skin) => {
 };
 
 const SlidesPlayer = (props, context) => {
-  const {step, question, cta, help, answer, buttons} = props;
+  const {step, question, cta, help, answer, buttons, clue} = props;
   const {skin, translate = identity} = context;
 
   const helpView = help ? <div className={style.helpView}>{help}</div> : null;
 
-  const answerView = createAnswerView(answer);
   const stepView = createStepView(step, skin);
+
+  let content = '';
+  let contentStyle = style.contentWrapper;
+  if (answer) {
+    const answerView = createAnswerView(answer);
+    content = (
+      <div className={style.answerWrapper}>
+        {answerView}
+      </div>
+    );
+  }
+
+  if (clue) {
+    contentStyle = style.contentWrapperHelpers;
+    content = (
+      <div className={style.clue}>
+        <div className={style.icon}>
+          <span className={style.iconClue} />
+        </div>
+        <div className={style.clueText}>{clue}</div>
+      </div>
+    );
+  }
 
   return (
     <div className={style.wrapper}>
@@ -80,14 +102,12 @@ const SlidesPlayer = (props, context) => {
       <div className={style.guideWrapper}>
         <span>{translate('New media')}</span>
       </div>
-      <div className={style.contentWrapper}>
+      <div className={contentStyle}>
         <div className={style.question}>
           {question}
         </div>
         {helpView}
-        <div className={style.answerWrapper}>
-          {answerView}
-        </div>
+        {content}
         <div className={style.ctaWrapper}>
           <Cta className={style.cta} {...cta} />
         </div>
@@ -114,6 +134,7 @@ SlidesPlayer.propTypes = {
   answer: PropTypes.shape({
     type: PropTypes.oneOf(['picker', 'qcm', 'qcmImage', 'freeText', 'dropDown', 'range']).required
   }),
+  clue: PropTypes.string,
   cta: PropTypes.shape(Cta.propTypes).isRequired,
   buttons: SlidesFooter.propTypes.buttons
 };
--- packages/@coorpacademy-components/storybook/components.js
+++ packages/@coorpacademy-components/storybook/components.js
@@ -422,6 +422,7 @@ import SlidesFooterFixtureSelected from '../src/molecule/slides/slides-footer/te
 import SlidesHeaderFixtureDefault from '../src/molecule/slides/slides-header/test/fixtures/default';
 import SlidesHeaderFixtureFreerun from '../src/molecule/slides/slides-header/test/fixtures/freerun';
 import SlidesHeaderFixtureNoSecondary from '../src/molecule/slides/slides-header/test/fixtures/no-secondary';
+import SlidesPlayerFixtureClue from '../src/molecule/slides/slides-player/test/fixtures/clue';
 import SlidesPlayerFixtureDefault from '../src/molecule/slides/slides-player/test/fixtures/default';
 import SlidesPlayerFixtureDropDown from '../src/molecule/slides/slides-player/test/fixtures/drop-down';
 import SlidesPlayerFixtureFreeText from '../src/molecule/slides/slides-player/test/fixtures/free-text';
@@ -1215,6 +1216,7 @@ export const fixtures = {
       NoSecondary: SlidesHeaderFixtureNoSecondary
     },
     SlidesPlayer: {
+      Clue: SlidesPlayerFixtureClue,
       Default: SlidesPlayerFixtureDefault,
       DropDown: SlidesPlayerFixtureDropDown,
       FreeText: SlidesPlayerFixtureFreeText,
